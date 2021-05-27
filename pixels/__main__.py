19d1dbf81d27e54b22def32ee780be8759cf3bd7
PureFunctor <purefunctor@gmail.com>
2021-05-27 17:39:20 +0800

    Create module entry point

---
+++ pixels/__main__.py
@@ -0,0 +1,23 @@
+import asyncio
+from os import getenv
+
+from dotenv import load_dotenv
+
+from pixels.client import Client
+
+
+async def main() -> None:
+
+    token = getenv("PIXELS_TOKEN")
+    if token is None:
+        raise RuntimeError("no token found")
+
+    async with Client(token) as client:
+        size = await client.get_size()
+        canvas = await client.get_pixels(size)
+        canvas.to_image().show()
+
+
+if __name__ == "__main__":
+    load_dotenv(".env")
+    asyncio.run(main())
